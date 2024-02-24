c93f2e4c957170fea5d252e1d316e7c044540af5
AndiPamungkas29 <andipamungkaasss29@gmail.com>
2024-02-24 22:41:44 +0700

    codelab modul 1

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Masukkan nama: ");
+        System.out.print("Masukkan nama Casis: ");
         String nama = scanner.nextLine();
 
         System.out.print("Masukkan jenis kelamin (L/P): ");
         String jenisKelaminInput = scanner.nextLine();
         char jenisKelamin = jenisKelaminInput.charAt(0);
 
-        System.out.print("Masukkan tanggal lahir (YYYY-MM-DD): ");
-        String tanggalLahirInput = scanner.nextLine();
-        LocalDate tanggalLahir = LocalDate.parse(tanggalLahirInput);
+        System.out.print("Masukkan data lahir (Tahun-Bulan-Hari): ");
+        String dataLahirInput = scanner.nextLine();
+        LocalDate dataLahir = LocalDate.parse(dataLahirInput);
 
         LocalDate today = LocalDate.now();
-        Period period = Period.between(tanggalLahir, today);
+        Period period = Period.between(dataLahir, today);
         int umur = period.getYears();
 
-        System.out.println("\n=== Output ===");
+        System.out.println("\n------ Output ------");
         System.out.println("Nama: " + nama);
         System.out.println("Jenis Kelamin: " + jenisKelamin);
         System.out.println("Umur: " + umur);
